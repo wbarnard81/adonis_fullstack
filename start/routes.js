0bbd2b253ec52cf51d5cc0cb524bcadbad5f79f4
Werner Barnard <wbarnard81@gmail.com>
2019-03-29 07:30:53 +0200

    Basic functionally is done

---
+++ start/routes.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route');
+
+//Route.on('/').render('index');
+
+Route.get('/', 'JobController.home');
+
+Route.on('/signup').render('auth.signup');
+Route.on('/login').render('auth.login');
+
+Route.on('/signup').render('auth.signup');
+Route.post('/signup', 'UserController.create').validator('CreateUser');
+
+Route.get('/logout', async ({ auth, response }) => {
+  await auth.logout();
+  return response.redirect('/');
+});
+
+Route.post('/login', 'UserController.login').validator('LoginUser');
+
+Route.get('/post-a-job', 'JobController.userIndex');
+Route.get('/post-a-job/delete/:id', 'JobController.delete');
+Route.get('/post-a-job/edit/:id', 'JobController.edit');
+Route.post('/post-a-job/', 'JobController.create').validator('CreateJob');
+Route.post('/post-a-job/update/:id', 'JobController.update').validator(
+  'CreateJob'
+);
